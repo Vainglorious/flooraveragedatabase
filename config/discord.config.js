eb0636b0ae75ada83af5c7bb54b0ff021d56f6af
Arda <something@gmail.com>
2021-12-04 13:13:23 -0800

    updated webhook

--- config/discord.config.js
+++ config/discord.config.js
@@ -1,4 +1,4 @@
 const webhook =
-  "https://discord.com/api/webhooks/916410003827593236/GdgwR2VEIQJhtKqYtEVIZ8fbi4PZiJoD8z6y_xi5HiRNfD1xaPKPoKAjWQwo0L3SsAoK";
+  "https://discord.com/api/webhooks/916796586040889374/eWyjleSgKnm2IrDXaEK0upOwOtrJ00BRIu7iZhyYxvywT3MQgh5TYqP27lQoYuW3";
 
 export default webhook;
