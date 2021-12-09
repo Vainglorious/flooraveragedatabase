874d570208d80241ef07d40f7277f88f116e65bd
Arda Örkin <ardaorkin@gmail.com>
2021-12-09 23:09:59 +0300

    extend test

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -1,9 +1,30 @@
 import openseaAPICall from "../assets/scripts";
-for (let index = 0; index < 99; index++) {
-  setTimeout(() => openseaAPICall(index), index * 1000);
-}
-setInterval(() => {
+import db from "../db/models";
+
+const getFunc = () => {
   for (let index = 0; index < 99; index++) {
-    setTimeout(() => openseaAPICall(index), index * 1000);
+    setTimeout(() => {
+      try {
+        openseaAPICall(index);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        } else {
+          console.log(error);
+        }
+      }
+    }, index * 1000);
   }
-}, 105);
+};
+
+setInterval(() => getFunc(), 105);
+
+async function getUpdatedAt(params) {
+  const updatedAts = await db.Tokens.findAll({
+    attributes: ["token_id", "createdAt", "updatedAt"],
+  });
+  console.log(updatedAts);
+}
+
+getFunc();
+getUpdatedAt();
